11b079ceaff32d0464af584d5da06523c9ea98b8
joyjjjz <joyjjjz@126.com>
2018-04-03 09:27:45 +0800

    添加UiSample.laya

--- my_legend/src/LayaSample.ts
+++ my_legend/src/LayaSample.ts
@@ -2,7 +2,15 @@
 class GameMain{
     constructor()
     {
-        Laya.init(600,400);
+        //创建舞台，默认背景色是黑色的
+        Laya.init(600, 300); 
+        var txt = new Laya.Text(); 
+        //设置文本内容
+        txt.text = "Hello Layabox";  
+        //设置文本颜色为白色，默认颜色为黑色
+        txt.color = "#ffffff";  
+        //将文本内容添加到舞台 
+        Laya.stage.addChild(txt);
     }
 }
 new GameMain();

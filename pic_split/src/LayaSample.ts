20a92303febd3009bee84d4eee16b46602fbc4b7
joyjjjjz <joyjjjz@126.com>
2018-04-08 15:45:16 +0800

    异步加载图集，并使用小图

--- pic_split/src/LayaSample.ts
+++ pic_split/src/LayaSample.ts
@@ -4,24 +4,74 @@ import Texture = laya.resource.Texture
 import HTMLCanvas = laya.resource.HTMLCanvas
 import Browser = laya.utils.Browser
 import WebGL = laya.webgl.WebGL;
+import Handler = laya.utils.Handler
      
 class GameMain{
     private sp: Sprite;
+    private srcFile: string = "res/atlas/comp/scene/login"
+    private prefix: string
+    private index:number = 0
+    private datas = []
+
     constructor()
     {
-        //初始化引擎
         Laya.init(Browser.width, Browser.height,WebGL);
-        //设置背景颜色
         Laya.stage.bgColor = "#ffcccc";
-        //设置舞台CLICK，该CLICK作为截屏的开关，点击舞台，对舞台对应的canvas区域进行截屏
-        Laya.stage.on(laya.events.Event.CLICK, this, this.onClick);
-        //随意绘制显示对象
+        Laya.stage.on(laya.events.Event.CLICK, this, this.onClick); 
+        Laya.loader.load(this.srcFile + ".json", Handler.create(this, this.onJsonLoaded), null, Laya.Loader.JSON);
+    }
+        
+    onJsonLoaded():void
+    {
+        const path = this.srcFile + ".json"
+        const json:JSON=Laya.loader.getRes(path);
+        const datas = json["frames"]
+        for (let pic in datas)
+        {
+            const data = datas[pic]
+            const sourceSize = data.sourceSize
+            const h=sourceSize.h, w=sourceSize.w
+            const add = [pic, h, w]
+            this.datas.push(add)
+            console.log("add data:[pic, h, w]:", pic, h, w)
+        }
+        this.prefix = json["meta"]["prefix"]
+
+        Laya.loader.clearRes(path)
+        Laya.loader.load(path, Handler.create(this, this.onPicLoaded), null, Laya.Loader.ATLAS);
+    }
+
+    onPicLoaded():void
+    {
+        console.log("on pic loaded", this.srcFile)
+        this.index = 0
+        this.checkCreateFile()
+    }
+
+    checkCreateFile():void
+    {
+        if(this.index >= this.datas.length)
+            return
+        
+        const data = this.datas[this.index]
+        const pic = data[0]
+        const h = data[1]
+        const w = data[2]
+        var loadPic 
+        // loadPic = "comp/scene/login/btn_enter.png"
+        loadPic = this.prefix + pic
         this.sp = new Sprite();
-        this.sp.loadImage("../src_dir/comp/scene/login.png");
+        this.sp.loadImage(loadPic);
+        console.log("load image:", loadPic)
         Laya.stage.addChild(this.sp); 
 
-        Laya.timer.once(100, this, this.onClick);        
-    }
+        var img:Laya.Image = new Laya.Image();
+        img.skin = loadPic;
+        //添加到舞台上显示
+        // Laya.stage.addChild(img);
+
+        Laya.timer.once(100, this, this.onClick);    
+    }           
 
     onClick():void
     {
@@ -30,12 +80,6 @@ class GameMain{
         var htmlC:HTMLCanvas = this.sp.drawToCanvas(300,300,0,0);
         //获取截屏区域的texture
         var _texture:Texture = new Texture(htmlC);
-            
-        //将截屏的texture进行draw绘制并显示到舞台
-        var sp2:Sprite = new Sprite();
-        sp2.x = 300;
-        sp2.graphics.drawTexture(_texture,0,0,300,300);
-        // Laya.stage.addChild(sp2);
 
         var a = document.createElement("a");
         a.href = htmlC.getCanvas().toDataURL();

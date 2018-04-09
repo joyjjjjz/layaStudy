a480213e1e45e5ce0f4a6d7c879ea7537f443eed
joyjjjjz <joyjjjz@126.com>
2018-04-09 13:05:26 +0800

    结合py脚本，批量生成小图

--- pic_split/src/LayaSample.ts
+++ pic_split/src/LayaSample.ts
@@ -8,9 +8,13 @@ import Handler = laya.utils.Handler
      
 class GameMain{
     private sp: Sprite;
+    private srcData: string = "res/atlas/srcList.data"
+    private srcFileList = []
+    private fileIndex:number = 0
+
     private srcFile: string = "res/atlas/comp/scene/login"
     private prefix: string
-    private index:number = 0
+    private picIndex:number = 0
     private datas = []
 
     constructor()
@@ -18,11 +22,35 @@ class GameMain{
         Laya.init(Browser.width, Browser.height,WebGL);
         Laya.stage.bgColor = "#ffcccc";
         Laya.stage.on(laya.events.Event.CLICK, this, this.onClick); 
+       
+        Laya.loader.load(this.srcData, Handler.create(this, this.onSrcDataLoaded), null, Laya.Loader.TEXT);
+    }
+
+    onSrcDataLoaded():void
+    {
+        const strData = Laya.loader.getRes(this.srcData)
+        this.srcFileList = strData.split("\n")
+        this.fileIndex = 0
+
+        this.checkLoadJson()
+    }
+
+    checkLoadJson():void
+    {
+        if(this.fileIndex >= this.srcFileList.length - 1)
+            return
+
+        this.srcFile = this.srcFileList[this.fileIndex]
+        // console.log("*******check load json:", this.fileIndex, this.srcFileList.length, this.srcFile)
+        this.fileIndex = this.fileIndex + 1
+
         Laya.loader.load(this.srcFile + ".json", Handler.create(this, this.onJsonLoaded), null, Laya.Loader.JSON);
     }
         
     onJsonLoaded():void
     {
+        this.datas = []
+
         const path = this.srcFile + ".json"
         const json:JSON=Laya.loader.getRes(path);
         const datas = json["frames"]
@@ -33,7 +61,7 @@ class GameMain{
             const h=sourceSize.h, w=sourceSize.w
             const add = [pic, h, w]
             this.datas.push(add)
-            console.log("add data:[pic, h, w]:", pic, h, w)
+            // console.log("add data:[pic, h, w]:", pic, h, w)
         }
         this.prefix = json["meta"]["prefix"]
 
@@ -43,17 +71,20 @@ class GameMain{
 
     onPicLoaded():void
     {
-        console.log("on pic loaded", this.srcFile)
-        this.index = 0
+        console.log("==========on pic loaded", this.srcFile)
+        this.picIndex = 0
         this.checkCreateFile()
     }
 
     checkCreateFile():void
     {
-        if(this.index >= this.datas.length)
+        if(this.picIndex >= this.datas.length)
+        {
+            this.checkLoadJson()
             return
+        }
         
-        const data = this.datas[this.index]
+        const data = this.datas[this.picIndex]
         const loadPic = this.prefix + data[0]
         this.sp = new Sprite();
         this.sp.loadImage(loadPic);
@@ -67,11 +98,11 @@ class GameMain{
     {
         //HTMLCanvas 是 Html Canvas 的代理类，封装了 Canvas 的属性和方法。。请不要直接使用 new HTMLCanvas！
         //此处将canvas指定区域进行截屏
-        const data = this.datas[this.index]
+        const data = this.datas[this.picIndex]
         const pic = data[0]
         const h = data[1]
         const w = data[2]
-        const loadPic = this.prefix + pic
+        const loadPic = this.srcFile + "/" + pic
 
         var htmlC:HTMLCanvas = this.sp.drawToCanvas(w,h,0,0);
         //获取截屏区域的texture
@@ -82,7 +113,7 @@ class GameMain{
         a.download = loadPic;
         a.click();    
 
-        this.index = this.index + 1
+        this.picIndex = this.picIndex + 1
         this.checkCreateFile()    
     }    
 }
